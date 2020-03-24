736ef4e386d46536921d4420d44af6ff76b4f0b7
sinhaamisha09 <sinhaamisha09@gmail.com>
2020-03-25 00:27:47 +0530

    frontend done

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,36 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
 	return (
 		<div>
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			  <a className="navbar-brand" href="#">Movies App</a>
-			  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			    <span className="navbar-toggler-icon"></span>
-			  </button>
-
-			  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-			    <ul className="navbar-nav mr-auto">
-			      <li className="nav-item active">
-			        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-			      </li>
+			<nav className="navbar navbar-light bg-dark mb-5">
+			
+			  <div className="container">
+			     <div className="navbar-header">
+		            <Link className="navbar-brand text-white text-lg brand-text" to="/">
+		              Movies App
+		            </Link>
+		          </div>
+			     <ul className="navbar-nav ml-auto text-white text-lg brand-text d-inline-block">
+			   
+			      <li className="nav-item d-inline-block mr-4">
+		              <i className="fab fa-imdb fa-5x" id="imdb-logo" />
+		          </li>
 		
-			      <li className="nav-item dropdown">
-			        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			          Dropdown
-			        </a>
-			        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-			          <a className="dropdown-item" href="#">Action</a>
-			          <a className="dropdown-item" href="#">Another action</a>
-			          <div className="dropdown-divider"></div>
-			          <a className="dropdown-item" href="#">Something else here</a>
-			        </div>
-			      </li>
 			    </ul>
 			  </div>
 			</nav>
 		</div>
-	)
+	);
 };
 
 export default Navbar;
+
+
+ // <li className="nav-item dropdown">
+	// 		        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	// 		          Dropdown
+	// 		        </a>
+	// 		        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+	// 		          <a className="dropdown-item" href="#">Action</a>
+	// 		          <a className="dropdown-item" href="#">Another action</a>
+	// 		          <div className="dropdown-divider"></div>
+	// 		          <a className="dropdown-item" href="#">Something else here</a>
+	// 		        </div>
+	// 		      </li>
